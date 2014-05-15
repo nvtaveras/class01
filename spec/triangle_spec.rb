e02b15e0079ea26c5d32d7b1a880569b180ae046
Nelson Taveras <ntaveras03@gmail.com>
2014-05-15 12:07:19 -0400

    Implemented some exaples

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,8 +3,17 @@ require 'triangle'
 
 describe Triangle do
 	
-	before do
-		@triangle = Triangle.new
+	describe "#kind" do
+
+		context "with correct params" do
+			it "should return the type of triangle" do
+				t = Triangle.new(1, 1, 1);
+				t.kind().should == "Equilateral"
+				t = Triangle.new(4, 4, 5);
+				t.kind().should == "Isosceles"
+			end
+		end
+
 	end
 
 end
